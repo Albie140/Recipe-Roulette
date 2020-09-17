3fcfcedc2cef5b7e86e1818e9a44096761bcb4b7
Alberte Laventure <alberte.laventure@gmail.com>
2020-09-17 10:40:20 -0400

    updated script file

---
+++ script.js
@@ -0,0 +1,17 @@
+//When the search button is clicked, run this function
+//Create a variable called queryURL which points to an API endpoint for search, Add url and apiKey.
+//Execute the AJAX function that will use the method GET and will retrieve a JSON object from the queryURL
+//.Then function for it to return the JSON object as the given argument.
+//Set a variable for the recipe/meal name, ingredients, image, recipe?, cooktime? 
+//set image tag assigned to variable 
+//assign the imageURL to the src from above to the new img element. Also set alt attribute
+//Append it to the card 
+
+//When the search button is clicked, run this function 
+//Create a variable called queryURL which points to an API endpoint for search, Add url and apiKey. 
+//Execute the AJAX function that will use the method GET and will retrieve a JSON object from the queryURL 
+//.Then function for it to return the JSON object as the given argument.
+//Set a variable for the drink/cocktail name, ingredients, image? 
+//set image tag assigned to variable 
+//assign the imageURL to the src from above to the new img element. Also set alt attribute 
+//  Append it to the card 
